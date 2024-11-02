cc7ade86da5a4a3eda62719ec38266f2f8d87f16
Abdul Rafay <amirrafay135@gmail.com>
2024-11-02 23:44:11 +0500

    test

--- src/app/components/tool.js
+++ src/app/components/tool.js
@@ -99,7 +99,6 @@ const Tool = () => {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
-  
 
   const handleFileChange = async (e) => {
     const file = e.target.files[0];
